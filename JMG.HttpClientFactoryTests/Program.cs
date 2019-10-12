00b33218d138cf4e14180b437e23c39e698d07c7
Jacek Glen <JacekGlen@users.noreply.github.com>
2019-10-12 14:50:54 +0100

    Add performance tests

---
+++ JMG.HttpClientFactoryTests/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JMG.HttpClientFactoryTests
+{
+    public class Program
+    {
+        public static void Main(params string[] args)
+        {
+
+        }
+    }
+}
--- PortExhaustionPresentationFramework/Program.cs
+++ PortExhaustionPresentationFramework/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Net;
 using System.Net.Http;
+using System.Diagnostics;
 
 namespace PortExhaustionPresentationFramework
 {
@@ -13,13 +14,36 @@ namespace PortExhaustionPresentationFramework
         static void Main(string[] args)
         {
             var server = new Server();
-            var exec = new HttpClientExecutor();
+
+            MeasureTimes().Wait();
+
+            //var exec = new HttpClientExecutor();
 
             //exec.Run(exec.Request, 10).Wait();
             //exec.Run(exec.RequestAndDispose, 10).Wait();
 
-            Console.WriteLine("Hey, I'm done with " + exec.errorsCount + " errors");
+            //Console.WriteLine("Hey, I'm done with " + exec.errorsCount + " errors");
             Console.ReadLine();
+
+            server.Close();
+        }
+
+        private static async Task MeasureTimes()
+        {
+            long elapsed;
+            var exec = new HttpClientExecutor();
+
+            //warm up
+            await exec.RunMeasured(exec.Request, 10);
+
+            elapsed = await exec.RunMeasured(exec.Request, 100);
+            Console.WriteLine($"Simple request time: {elapsed}ms");
+
+            elapsed = await exec.RunMeasured(exec.RequestAndDispose, 100);
+            Console.WriteLine($"Request and dispose time: {elapsed}ms");
+
+            elapsed = await exec.RunMeasured(exec.RequestWithStatic, 100);
+            Console.WriteLine($"Request using singleton time: {elapsed}ms");
         }
     }
 
@@ -41,6 +65,11 @@ namespace PortExhaustionPresentationFramework
             listener.BeginGetContext(Callback, null);
         }
 
+        public void Close()
+        {
+            listener.Close();
+        }
+
         static void Callback(IAsyncResult ar)
         {
             var context = listener.EndGetContext(ar);
@@ -60,6 +89,7 @@ namespace PortExhaustionPresentationFramework
     public class HttpClientExecutor
     {
         public int errorsCount = 0;
+        Stopwatch sw = new Stopwatch();
 
         public async Task Run(Func<Task> method, int iterationsCount = 10)
         {
@@ -81,6 +111,25 @@ namespace PortExhaustionPresentationFramework
                     }
                 }
             }
+        }
+
+        public async Task<long> RunMeasured(Func<Task> method, int iterationsCount = 10)
+        {
+            sw.Restart();
+
+            for (int i = 1; i <= iterationsCount; ++i)
+            {
+                try
+                {
+                    await method();
+                }
+                catch (Exception ex)
+                {
+                }
+            }
+
+            sw.Stop();
+            return sw.ElapsedMilliseconds;
         }
 
         private string url = "http://localhost:3070";
